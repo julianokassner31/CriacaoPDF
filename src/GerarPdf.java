3c16742559ba55c40f68fa1cc3c6d04fc52b1ca2
Juliano Kassner <stoplanhousemt@gmail.com>
2018-01-22 09:50:14 -0300

    adicionando projeto

---
+++ src/GerarPdf.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfWriter;
+
+public class GerarPdf {
+	public static void main(String[] args) throws FileNotFoundException, DocumentException {
+//		Document document = new Document(PageSize.A4,50,50,50,50);
+//		PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("C:\\ITextTest.pdf"));
+//		document.open();
+//		document.add(new Paragraph("First page of the document."));
+//		document.close();
+		
+//		String property = System.getProperty("user.name");
+//		System.out.println(property);
+		
+	}
+}
